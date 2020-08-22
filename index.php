698f31cf4eaff3b60296415276d95487d12da7ee
Ваше Имя <you@example.com>
2020-08-22 20:19:20 +0300

    1

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
-<head>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+  <head>
+  <title>Анатолий Домрачев</title>
   <meta charset="utf-8">
-  <title>Тест 1 курс</title>
   <link href="style.css" rel="stylesheet">
- </head>
+  </head>
+  <body>
 
-<H2>Тест 1 курс</H2>
+<div class = header id = header>
+</div>
+<div class = left id = left>
+</div>
+<div class = center id = center>
+</div>
+<div class = footer id = footer>
+</div>
+
+<script type="text/javascript" src="jquery.min.js"></script>
+<script type="text/javascript" src="ftc.js"></script>
+
+<script>
+
+ftc( 'header.php' , 'header' );
+ftc( 'left.php' , 'left' );
+ftc( 'center.php' , 'center' );
+
+</script>
 
---
+++ test/index.php
@@ -0,0 +1,8 @@
+<head>
+  <meta charset="utf-8">
+  <title>Тест 1 курс</title>
+  <link href="style.css" rel="stylesheet">
+ </head>
+
+<H2>Тест 1 курс</H2>
+
